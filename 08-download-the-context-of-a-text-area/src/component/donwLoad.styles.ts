5d1e752f7f00d277b25d778f298c2fa8fc54e6ca
Félix Franco <felix.franco.rojas@gmail.com>
2022-05-20 22:49:38 +0200

    Readme file with the instructions

---
+++ 08-download-the-context-of-a-text-area/src/component/donwLoad.styles.ts
@@ -0,0 +1,9 @@
+import { css } from "@emotion/css";
+
+export const textArea = css`
+  width: max-content;
+`;
+
+export const downLoadButton = css`
+  margin-top: 1rem;
+`;
---
+++ 08-download-the-context-of-a-text-area/src/component/downLoad-component.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Button from "@mui/material/Button";
+
+import * as classes from "./donwLoad.styles";
+
+export const DownLoadComponent: React.FC = () => {
+  const [text, setText] = React.useState<string>("");
+
+  const handleClick = (e) => {
+    const popup = confirm("¿Seguro que quieres descargar el archivo?");
+    if (popup) {
+      const a = document.createElement("a");
+      const file = new Blob([text], { type: "text/plain" });
+      const url = URL.createObjectURL(file);
+      a.href = url;
+      a.download = "text.txt";
+      a.click();
+      URL.revokeObjectURL(url);
+    }
+  };
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+  console.log("se ha recargado la página");
+  return (
+    <>
+      <div>
+        <h1>You can download the text in the frame</h1>
+      </div>
+      <main className={classes.textArea}>
+        <textarea
+          cols={40}
+          rows={10}
+          defaultValue={text}
+          onChange={handleChange}
+        ></textarea>
+      </main>
+      <section className={classes.downLoadButton}>
+        <Button variant="contained" component="span" onClick={handleClick}>
+          Convert to txt
+        </Button>
+      </section>
+    </>
+  );
+};
